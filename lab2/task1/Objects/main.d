46798b560a2e1061a25c32b27b6bffd6ebf205c8
Sebastian De Leon <sebastianipl0131@gmail.com>
2026-03-02 10:58:35 -0500

    remove files supposed to be ignored

--- lab2/task1/Objects/main.d
+++
@@ -1,4 +0,0 @@
-./objects/main.o: main.c \
-  C:\Users\sebas\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.7.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\stm32l476xx.h \
-  C:\Users\sebas\AppData\Local\Arm\Packs\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\sebas\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.7.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
--- lab2/task5/Objects/system_stm32l4xx.d
+++
@@ -1,5 +0,0 @@
-./objects/system_stm32l4xx.o: RTE\Device\STM32L476RGTx\system_stm32l4xx.c \
-  C:\Users\sebas\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.7.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\stm32l4xx.h \
-  C:\Users\sebas\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.7.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\stm32l476xx.h \
-  C:\Users\sebas\AppData\Local\Arm\Packs\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\sebas\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.7.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
